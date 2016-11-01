d0fbc2a1da298bbeeb5c91f22678967c2535f0c4
eersin <eersin@mmsofts.com>
2016-11-01 23:31:54 +0700

    general renaming again

--- aapp/src/main/java/com/github/ersin_ertan/aapp/MainActivity.java
+++ aapp/src/main/java/com/github/ersin_ertan/aapp/MainActivity.java
@@ -1,16 +1,12 @@
 package com.github.ersin_ertan.aapp;
 
+import android.app.Activity;
 import android.os.Bundle;
-import android.support.v7.app.AppCompatActivity;
-import com.github.ersin_ertan.a.A;
-import com.mmsofts.alib.R;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends Activity {
 
   @Override protected void onCreate(Bundle savedInstanceState) {
     super.onCreate(savedInstanceState);
     setContentView(R.layout.activity_main);
-
-    A.toast(this, String.valueOf(A.isInetConnected(MainActivity.this)));
   }
 }
