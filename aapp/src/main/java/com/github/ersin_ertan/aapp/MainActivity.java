4aad4c728aa485f3cb003fcdef2ca77eb089ca00
eersin <eersin@mmsofts.com>
2016-11-02 10:16:00 +0700

    Update examples and api version

--- aapp/src/main/java/com/github/ersin_ertan/aapp/MainActivity.java
+++ aapp/src/main/java/com/github/ersin_ertan/aapp/MainActivity.java
@@ -1,7 +1,11 @@
 package com.github.ersin_ertan.aapp;
 
 import android.app.Activity;
+import android.app.AlertDialog;
+import android.app.ProgressDialog;
 import android.os.Bundle;
+import com.github.ersin_ertan.a.Is;
+import com.github.ersin_ertan.a.Show;
 
 public class MainActivity extends Activity {
 
@@ -9,5 +13,15 @@ public class MainActivity extends Activity {
     super.onCreate(savedInstanceState);
     setContentView(R.layout.activity_main);
 
+    Show.toast(this, android.R.string.untitled);
+
+    Show.alertDialog(new AlertDialog.Builder(this), "Hello", "World");
+
+    Show.alertDialogList(new AlertDialog.Builder(this), "title", new CharSequence[] { "a" }, null);
+
+    ProgressDialog pd = Show.progressDialog(this, String.valueOf(Is.inetConnected(this)));
+    pd.cancel();
+
+    Is.inetConnected(this);
   }
 }
