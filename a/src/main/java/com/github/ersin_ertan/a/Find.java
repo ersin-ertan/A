490188520aadb137aa07ba3f1c0e2dc4c5d8db11
eersin <eersin@mmsofts.com>
2016-12-20 00:05:41 +0700

    Update: - manifest to remove rtl and label

--- a/src/main/java/com/github/ersin_ertan/a/Find.java
+++ a/src/main/java/com/github/ersin_ertan/a/Find.java
@@ -1,7 +1,5 @@
 package com.github.ersin_ertan.a;
 
-import android.support.annotation.NonNull;
-
 import static com.github.ersin_ertan.a.internal.ErrorExceptionMessages.ILLEGAL_INSTANCE;
 
 /**
@@ -14,28 +12,28 @@ public final class Find {
     throw new IllegalStateException(ILLEGAL_INSTANCE);
   }
 
-  public static int changeIndex(@NonNull final String a, @NonNull final String b) {
-    if (a.equals(b)) return -1;
-    int aLen = a.length();
-    int bLen = b.length();
-    int goTil = aLen > bLen ? aLen : bLen;
-    if (goTil == 1 && !a.equals(b)) return 0;
-    for (int i = 0; i < goTil; i++) {
-      char cA, cB;
-      try {
-        cA = a.charAt(i);
-      } catch (IndexOutOfBoundsException ioobe) {
-        if (a.charAt(i - 1) == b.charAt(i)) --i;
-        return i;
-      }
-      try {
-        cB = b.charAt(i);
-      } catch (IndexOutOfBoundsException ioobe) {
-        if (b.charAt(i - 1) == a.charAt(i)) --i;
-        return i;
-      }
-      if (cA != cB) return i;
-    }
-    return -1;
-  }
+  //public static int changeIndex(@NonNull final String a, @NonNull final String b) {
+  //  if (a.equals(b)) return -1;
+  //  int aLen = a.length();
+  //  int bLen = b.length();
+  //  int goTil = aLen > bLen ? aLen : bLen;
+  //  if (goTil == 1 && !a.equals(b)) return 0;
+  //  for (int i = 0; i < goTil; i++) {
+  //    char cA, cB;
+  //    try {
+  //      cA = a.charAt(i);
+  //    } catch (IndexOutOfBoundsException ioobe) {
+  //      if (a.charAt(i - 1) == b.charAt(i)) --i;
+  //      return i;
+  //    }
+  //    try {
+  //      cB = b.charAt(i);
+  //    } catch (IndexOutOfBoundsException ioobe) {
+  //      if (b.charAt(i - 1) == a.charAt(i)) --i;
+  //      return i;
+  //    }
+  //    if (cA != cB) return i;
+  //  }
+  //  return -1;
+  //}
 }
